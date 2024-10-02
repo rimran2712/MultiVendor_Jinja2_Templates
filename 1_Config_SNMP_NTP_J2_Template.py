e5f712b78cc825823858341e85bb3de9d0ec8bf2
Imran <rimran84@yahoo.com>
2024-10-02 00:49:34 -0500

    added snmp and NTP J2 template

--- 1_Config_SNMP_NTP_J2_Template.py
+++ 1_Config_SNMP_NTP_J2_Template.py
@@ -1,12 +1,22 @@
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
+from nornir_scrapli.tasks import send_configs
+from nornir_jinja2.plugins.tasks import template_file
 import os
 from rich import print
 
 
-nr = InitNornir (config_file="/home/imran/Documents/Automation/Nornir/Runbooks_Repositories/MultiVendor_Jinja2_Templates/Inventory/config.yaml")
+nr = InitNornir (config_file="Inventory/config.yaml")
 
 # Clearing the Screen
 os.system('clear')
 
+def config_snmp_ntp_j2_template(task):
+    snmp_ntp_template = task.run (task=template_file, template="snmp_ntp.j2", path=f"J2_Templates/{task.host.platform}")
+    task.host['snmp_ntp'] = snmp_ntp_template.result
+    snmp_ntp_rendered = task.host['snmp_ntp']
+    snmp_ntp_config = snmp_ntp_rendered.splitlines()
+    task.run (task=send_configs, configs=snmp_ntp_config)
 
+reseults = nr.run (task=config_snmp_ntp_j2_template)
+print_result (reseults)
